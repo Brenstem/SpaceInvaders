10d2ce829dac8138c26203ebd99f717f5004878e
Dane Nilsson <dane.nilsson@gmail.com>
2018-12-04 15:11:28 +0100

    Tried to fixed x spawning and tried to fix y spawning

--- Space Invaders/Assets/Scripts/EnemySpawner.cs
+++ Space Invaders/Assets/Scripts/EnemySpawner.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+
 public class EnemySpawner : MonoBehaviour
 {
 
@@ -19,18 +20,21 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField] Camera camera;
     [SerializeField] Transform startingSpawnPos;
+    [SerializeField] Transform EnemyHolder;
 
     //These two will need range keyword
     //[SerializeField] int rows; // not used atm
     [SerializeField] int columns;
     [SerializeField] float enemyBreathingRoom;
 
+
     Vector2 viewportSize;
     float xBounds;
 
     //debugging
     bool spawn = true;
-    int currentYPos;
+    float lastColliderSizeY = 0;
+    int currentRow;
 
     private void Start()
     {
@@ -39,10 +43,8 @@ public class EnemySpawner : MonoBehaviour
         InvokeRepeating("moveStatic", 0, 0.1f);
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        enemyBreathingRoom += 0.1f;
         if (spawn)
         {
             SpawnLightEnemies();
@@ -56,94 +58,122 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnLightEnemies()
     {
-        float colliderSizeX = lightEnemy.GetComponent<BoxCollider2D>().size.x 
+        float colliderSizeX = lightEnemy.GetComponent<BoxCollider2D>().size.x
             * lightEnemy.GetComponent<Transform>().localScale.x + enemyBreathingRoom;
 
-        Debug.Log(colliderSizeX - 0.5f);
-        lightEnemyRows += currentYPos;
-        SetStartPosition(lightEnemy);
+        float colliderSizeY = lightEnemy.GetComponent<BoxCollider2D>().size.y *
+            lightEnemy.GetComponent<Transform>().localScale.y;
 
-        for (int i = currentYPos; i < lightEnemyRows; i++)
+        lightEnemyRows += currentRow;
+        SetStartPosition(lightEnemy, columns);
+
+        for (int i = currentRow; i < lightEnemyRows; i++)
         {
-            Debug.Log(startingSpawnPos.position.x);
             Instantiate(lightEnemy, new Vector2(startingSpawnPos.position.x,
-                startingSpawnPos.position.y + i * colliderSizeX), Quaternion.identity, this.transform);
-            Debug.Log(startingSpawnPos.position.x);
-
+                startingSpawnPos.position.y + i * (colliderSizeY + lastColliderSizeY + enemyBreathingRoom) / 2), Quaternion.identity, EnemyHolder.transform);
 
             for (int j = 1; j < columns; j++)
             {
                 Instantiate(lightEnemy, new Vector2(startingSpawnPos.position.x + j * colliderSizeX,
-                    startingSpawnPos.position.y + i * colliderSizeX), Quaternion.identity, this.transform);
+                    startingSpawnPos.position.y + i * (colliderSizeY + lastColliderSizeY + enemyBreathingRoom) / 2), Quaternion.identity, EnemyHolder.transform);
             }
-            currentYPos++;
+            currentRow++;
+            lastColliderSizeY = colliderSizeY;
         }
     }
 
     private void SpawnQuickEnemies()
     {
-        float colliderSizeX = quickEnemy.GetComponent<BoxCollider2D>().size.x * quickEnemy.GetComponent<Transform>().localScale.x + enemyBreathingRoom;
-        quickEnemyRows += currentYPos;
-        SetStartPosition(quickEnemy);
+        float colliderSizeX = quickEnemy.GetComponent<BoxCollider2D>().size.x *
+            quickEnemy.GetComponent<Transform>().localScale.x + +enemyBreathingRoom;
 
-        for (int i = currentYPos; i < quickEnemyRows; i++)
+        float colliderSizeY = quickEnemy.GetComponent<BoxCollider2D>().size.y *
+            quickEnemy.GetComponent<Transform>().localScale.y;
+
+        quickEnemyRows += currentRow;
+        SetStartPosition(quickEnemy, columns);
+
+        for (int i = currentRow; i < quickEnemyRows; i++)
         {
             Instantiate(quickEnemy, new Vector2(startingSpawnPos.position.x,
-                startingSpawnPos.position.y + i * colliderSizeX), Quaternion.identity, this.transform);
+                startingSpawnPos.position.y + i * (colliderSizeY + lastColliderSizeY + enemyBreathingRoom) / 2), Quaternion.identity, EnemyHolder.transform);
 
             for (int j = 1; j < columns; j++)
             {
                 Instantiate(quickEnemy, new Vector2(startingSpawnPos.position.x + j * colliderSizeX,
-                    startingSpawnPos.position.y + i * colliderSizeX), Quaternion.identity, this.transform);
+                    startingSpawnPos.position.y + i * (colliderSizeY + lastColliderSizeY + enemyBreathingRoom) / 2), Quaternion.identity, EnemyHolder.transform);
             }
-            currentYPos++;
+
+            Debug.Log("last collider size" + lastColliderSizeY);
+            Debug.Log("collider size" + colliderSizeY);
+            Debug.Log("Breathing room " + enemyBreathingRoom);
+            Debug.Log("currentRow " + i);
+
+            Debug.Log(i * (colliderSizeY + lastColliderSizeY + enemyBreathingRoom) / 2);
+
+            currentRow++;
+            lastColliderSizeY = colliderSizeY;
         }
     }
 
     private void spawnProtectors()
     {
-        float colliderSizeX = protectorEnemy.GetComponent<BoxCollider2D>().size.x * protectorEnemy.GetComponent<Transform>().localScale.x + enemyBreathingRoom;
-        int protectorRows = currentYPos;
-        SetStartPosition(protectorEnemy);
+        float colliderSizeX = protectorEnemy.GetComponent<BoxCollider2D>().size.x *
+            protectorEnemy.GetComponent<Transform>().localScale.x + enemyBreathingRoom;
+
+        float colliderSizeY = protectorEnemy.GetComponent<BoxCollider2D>().size.y *
+    protectorEnemy.GetComponent<Transform>().localScale.y;
+
+        int protectorRows = currentRow;
+        SetStartPosition(protectorEnemy, protectorAmount * bossEnemyAmount);
 
         if (protectorAmount > 0)
             protectorRows++;
 
-        for (int i = currentYPos; i < protectorRows; i++)
+        for (int i = currentRow; i < protectorRows; i++)
         {
             Instantiate(protectorEnemy, new Vector2(startingSpawnPos.position.x,
-                startingSpawnPos.position.y + i * colliderSizeX), Quaternion.identity, this.transform);
+                startingSpawnPos.position.y + i * (colliderSizeY + lastColliderSizeY + enemyBreathingRoom) / 2), Quaternion.identity, EnemyHolder.transform);
 
             for (int j = 1; j < bossEnemyAmount * protectorAmount; j++)
             {
                 Instantiate(protectorEnemy, new Vector2(startingSpawnPos.position.x + j * colliderSizeX,
-                startingSpawnPos.position.y + i * colliderSizeX), Quaternion.identity, this.transform);
+                startingSpawnPos.position.y + i * (colliderSizeY + lastColliderSizeY + enemyBreathingRoom) / 2), Quaternion.identity, EnemyHolder.transform);
             }
-            currentYPos++;
+            currentRow++;
+            lastColliderSizeY = colliderSizeY;
         }
     }
 
     private void SpawnBossEnemies()
     {
-        float colliderSizeX = bossEnemy.GetComponent<BoxCollider2D>().size.x * bossEnemy.GetComponent<Transform>().localScale.x + enemyBreathingRoom;
-        int bossRows = currentYPos;
-        SetStartPosition(bossEnemy);
+        float colliderSizeX = bossEnemy.GetComponent<BoxCollider2D>().size.x
+            * bossEnemy.GetComponent<Transform>().localScale.x + enemyBreathingRoom;
+
+        float colliderSizeY = bossEnemy.GetComponent<BoxCollider2D>().size.y 
+            * bossEnemy.GetComponent<Transform>().localScale.y; 
+
+        int bossRows = currentRow;
+
+        SetStartPosition(bossEnemy, bossEnemyAmount);
 
         if (bossEnemyAmount > 0)
             bossRows++;
 
-        for (int i = currentYPos; i < bossRows; i++)
+        for (int i = currentRow; i < bossRows; i++)
         {
             Instantiate(bossEnemy, new Vector2(startingSpawnPos.position.x,
-                startingSpawnPos.position.y + i * colliderSizeX), Quaternion.identity, this.transform);
+                startingSpawnPos.position.y + i * (colliderSizeY + lastColliderSizeY + enemyBreathingRoom) / 2), Quaternion.identity, EnemyHolder.transform);
 
             for (int j = 1; j < bossEnemyAmount; j++)
             {
 
                 Instantiate(bossEnemy, new Vector2(startingSpawnPos.position.x + j * colliderSizeX,
-                    startingSpawnPos.position.y + i * colliderSizeX), Quaternion.identity, this.transform);
+                    startingSpawnPos.position.y + i * (colliderSizeY + lastColliderSizeY + enemyBreathingRoom) / 2), Quaternion.identity, EnemyHolder.transform);
             }
+            currentRow++;
         }
+        lastColliderSizeY = colliderSizeY;
     }
 
     private void moveStatic()
@@ -156,7 +186,7 @@ public class EnemySpawner : MonoBehaviour
                 this.transform.GetChild(i).GetComponent<Transform>().position.x <
                 -xBounds + this.transform.GetChild(i).GetComponent<BoxCollider2D>().size.x / 2)
             {
-                    check = true;
+                check = true;
             }
         }
 
@@ -169,17 +199,24 @@ public class EnemySpawner : MonoBehaviour
 
     private void ChangeDirection()
     {
-        for (int i = 0; i < this.transform.childCount; i++)
+        for (int i = 0; i < EnemyHolder.transform.childCount; i++)
         {
             this.transform.GetChild(i).GetComponent<EnemyController>().ChangeDirection();
         }
     }
 
-    private void SetStartPosition(GameObject enemyType)
+    private void SetStartPosition(GameObject enemyType, int columns)
     {
         float colliderSizeX = enemyType.GetComponent<BoxCollider2D>().size.x * enemyType.GetComponent<Transform>().localScale.x;
-        Vector2 formationSize = new Vector2(columns * colliderSizeX, 0);
 
-        startingSpawnPos.position = -formationSize/2;
+        //Debug.Log("collider size " + colliderSizeY / 2);
+        //Debug.Log("last collider size setstart" + lastColliderSizeY);
+        //Debug.Log("current row " + currentRow);
+
+        Vector2 formationSize = new Vector2(colliderSizeX * columns + enemyBreathingRoom * (columns - 1) - colliderSizeX, 0);
+
+        formationSize.x = -formationSize.x / 2;
+
+        startingSpawnPos.position = formationSize;
     }
 }
